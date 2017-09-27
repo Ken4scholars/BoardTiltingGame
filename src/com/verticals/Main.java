31c6efd206871402ee33738d8777abce63ea86d8
Kenneth Chinedu <kchinedu@mail.ru>
2017-09-27 10:42:09 +0300

    refactor main

--- src/com/verticals/Main.java
+++ src/com/verticals/Main.java
@@ -10,7 +10,6 @@ import java.io.IOException;
 import java.util.*;
 
 public class Main {
-    private static Queue<Board> boards = new ArrayDeque<>();
 
     public static void main(String[] args) throws IOException {
         if (args.length < 1) return;
